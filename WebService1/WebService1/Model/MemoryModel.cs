79d55c94df633f381d6fdf7638bb35d8a5ef89a0
Palaoa <785388744@qq.com>
2016-10-30 14:04:38 +0800

    Revert "Revert "WebTest""
    This reverts commit cc4df9f1531c1f45204e378623a1d55905058379.

---
+++ WebService1/WebService1/Model/MemoryModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace STWebService.Model
+{
+    public class MemoryModel
+    {
+        public string memory_id { get; set; }
+        public string user_id { get; set; }
+        public string title { get; set; }
+        public string content { get; set; }
+        public string photo { get; set; }
+        public string limitation { get; set; }
+    }
+}
