d9cbe25f9d475b36cf0f4c3c610baae05d3c2344
alina52 <alinawang1125@gmail.com>
2016-12-05 10:21:30 +0800

    Android UI

---
+++ Android UI/app/src/main/java/io/github/froger/instamaterial/InstaMaterialApplication.java
@@ -0,0 +1,13 @@
+package io.github.froger.instamaterial;
+
+import android.app.Application;
+
+import timber.log.Timber;
+
+public class InstaMaterialApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Timber.plant(new Timber.DebugTree());
+    }
+}
